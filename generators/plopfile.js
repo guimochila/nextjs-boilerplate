5d351c7b6745918d5dd8d7bbb9317769e2ae0b0b
Guilherme <gui.mochila@gmail.com>
2022-04-25 21:30:45 +0200

    feature: Add plop for files generation

---
+++ generators/plopfile.js
@@ -0,0 +1,41 @@
+module.exports = function (plop) {
+  plop.setGenerator('component', {
+    description: 'application component',
+
+    prompts: [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'Component name?',
+      },
+    ],
+
+    actions: [
+      {
+        type: 'add',
+        path: '../src/components/{{pascalCase name}}/index.tsx',
+        templateFile: 'templates/index.tsx.hbs',
+      },
+      {
+        type: 'add',
+        path: '../src/components/{{pascalCase name}}/{{pascalCase name}}.tsx',
+        templateFile: 'templates/component.tsx.hbs',
+      },
+      {
+        type: 'add',
+        path: '../src/components/{{pascalCase name}}/{{pascalCase name}}.stories.tsx',
+        templateFile: 'templates/stories.tsx.hbs',
+      },
+      {
+        type: 'add',
+        path: '../src/components/{{pascalCase name}}/styles.ts',
+        templateFile: 'templates/styles.ts.hbs',
+      },
+      {
+        type: 'add',
+        path: '../src/components/{{pascalCase name}}/test.tsx',
+        templateFile: 'templates/test.tsx.hbs',
+      },
+    ],
+  })
+}
